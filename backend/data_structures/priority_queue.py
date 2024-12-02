0f3458035777ebd9a2549c6f62b6c74c5db3bc5c
sairit <sai.ritvik67@gmail.com>
2024-12-01 22:21:44 -0500

    Finished implementing priority queue with a heap.

--- backend/data_structures/priority_queue.py
+++ backend/data_structures/priority_queue.py
@@ -4,14 +4,14 @@ class PriorityQueue:
     def __init__(self):
         # We should base the implementation off of a heap, since it is more efficient over a list. 
         # Make sure that you are inserting only tuples structured: (priority_weight (int), value (Any))
-        self.queue = []
+        self.queue = Heap()
     
     def is_empty(self):
         """
         Check if the priority queue is empty.
         :return: True if the queue is empty, False otherwise.
         """
-        pass
+        return self.queue.is_empty()
 
     def insert(self, item, priority):
         """
@@ -19,18 +19,26 @@ class PriorityQueue:
         :param item: The item to be inserted.
         :param priority: The priority of the item.
         """
-        pass
+        self.queue.insert((priority, item))
 
     def delete(self):
         """
         Remove and return the item with the highest priority.
         :return: The item with the highest priority.
         """
-        pass
+        top = self.queue.pop()
+        if top == None:
+            return None
+        
+        return top.get(1)
 
     def peek(self):
         """
         Return the item with the highest priority without removing it.
         :return: The item with the highest priority.
         """
-        pass
+        top = self.queue.peek()
+        if top == None:
+            return None
+        
+        return top.get(1)
