a89a7cbb5720417164f5e9544619d74f70f1a7d8
Arjun Rao <rao.rjun@gmail.com>
2024-12-02 11:58:09 -0500

    commented linked list

--- backend/data_structures/linked_list.py
+++ backend/data_structures/linked_list.py
@@ -1,13 +1,50 @@
 class Node:
+    """
+    A generic node class for a linked list
+
+    Attributes:
+        value: The value of the node
+        next: The next node in the linked list
+    """
+
     def __init__(self, value):
+        """
+        Initializes a node with a given value
+
+        Args:
+            value: The value for the node
+        """
+
         self.value = value
         self.next = None
 
+
+
+
 class LinkedList:
+    """
+    A generic linked list class
+
+    Attributes:
+        head: The first node in the linked list
+    """
+
+
     def __init__(self):
+        """
+        Initializes an empty linked list
+        """
+
         self.head = None
     
     def append(self, value):
+        """
+        Appends a new node with a given value to the end of the linked list
+
+        Args:
+            value: The value for the new node
+        """
+
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
@@ -17,11 +54,25 @@ class LinkedList:
         current.next = new_node
     
     def append_to_front(self, value):
+        """
+        Appends a new node with a given value to the front of the linked list
+
+        Args:
+            value: The value for the new node
+        """
+
         new_node = Node(value)
         new_node.next = self.head
         self.head = new_node
     
     def delete(self, value):
+        """
+        Deletes the first instance of a node with a given value from the linked list
+
+        Args:
+            value: The value of the node to delete
+        """
+
         if self.head is None:
             return
         
@@ -40,6 +91,16 @@ class LinkedList:
 
         
     def find(self, value):
+        """
+        Returns the first node with a given value
+
+        Args:
+            value: The value of the node to find
+
+        Returns:
+            The first node with the given value, or None if the value is not found
+        """
+
         current = self.head
         while current:
             if current.value == value:
@@ -48,6 +109,10 @@ class LinkedList:
         return None
     
     def print_list(self):
+        """
+        Prints the linked list
+        """
+
         current = self.head
         while current:
             print(current.value, end=" -> ")
